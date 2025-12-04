3cfb3c4840ec7b8d8abc3494708331c58f2604d4
Cuong <cuong.0972308862@gmail.com>
2025-12-05 03:15:55 +0700

    Fix asgi import order

--- myproject/asgi.py
+++ myproject/asgi.py
@@ -1,13 +1,20 @@
 import os
 from django.core.asgi import get_asgi_application
+
+# 1. Thiết lập biến môi trường ĐẦU TIÊN
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+
+# 2. Khởi tạo Django ASGI App ĐẦU TIÊN
+# Dòng này bắt buộc phải chạy trước khi import game.routing
+django_asgi_app = get_asgi_application()
+
+# 3. BÂY GIỜ mới được import các thư viện Channels và code của bạn
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import game.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
-
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app, # Sử dụng biến đã khởi tạo ở trên
     "websocket": AuthMiddlewareStack(
         URLRouter(
             game.routing.websocket_urlpatterns
