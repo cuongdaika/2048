2e049ad6ed14aad2965edb81ff629b3e686bf9f1
Cuong <cuong.0972308862@gmail.com>
2025-12-05 03:40:38 +0700

    Update build script to create admin

--- build.sh
+++ build.sh
@@ -4,11 +4,15 @@
 # Exit ngay lập tức nếu có lỗi xảy ra
 set -o errexit
 
-# Cài đặt gói (Render đã làm, nhưng thêm vào để đảm bảo)
+# Cài đặt gói
 pip install -r requirements.txt
 
-# Chạy migrate để tạo bảng
+# Chạy migrate để tạo bảng (quan trọng)
 python manage.py migrate
 
-# Thu thập Static files (Whitenoise)
+# Thu thập Static files
 python manage.py collectstatic --no-input
+
+# --- PHẦN THÊM MỚI ---
+# Chạy script tự động tạo Admin
+python create_superuser.py
